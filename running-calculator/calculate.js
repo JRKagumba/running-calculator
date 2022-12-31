e860a3fefc02f98e56b3a9abfd611344fbe10c61
JRKagumba <63820705+JRKagumba@users.noreply.github.com>
2022-12-31 08:54:42 -0500

    added functions to ensure correct d,t,p format

--- running-calculator/calculate.js
+++ running-calculator/calculate.js
@@ -1,12 +1,44 @@
 import * as convTools from 'convert.js';
 
-const dictionary = {
-    "Marathon": 42200,
-    "Half-Marathon": 21100
-}
+
+function ensureDistanceFormat(radioButton, textInput) {
+    if (!radioButton.checked) {
+      // select the radio button if it is not already selected
+      radioButton.checked = true;
+    }
+    if (textInput.value.trim() === "") {
+      // set the value of the text input to a default string if it is empty
+      textInput.value = "Please enter a value";
+    }
+  }
 
 
+function ensureTimeFormat(timeInput) {
+    // regular expression to match time in 'hh:mm:ss' or 'mm:ss' format
+    const timeFormat = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$|^[0-5]?[0-9]:[0-5][0-9]$/;
+  
+    if (!timeFormat.test(timeInput.value)) {
+      // set the value of the time input to a default string if it is not in the correct format
+      timeInput.value = "00:00:00";
+    }
+  }
 
+function ensurePaceFormat(input, type) {
+    if (type === "mins:sec/mile" || type === "mins:sec/km") {
+      // regular expression to match pace in 'mm:ss' format
+      const paceFormat = /^[0-5]?[0-9]:[0-5][0-9]$/;
+      if (!paceFormat.test(input.value)) {
+        // set the value of the input to a default string if it is not in the correct format
+        input.value = "00:00";
+      }
+    } else if (type === "kph" || type === "mph") {
+      if (isNaN(input.value)) {
+        // set the value of the input to a default number if it is not a valid number
+        input.value = 0;
+      }
+    }
+  }
+  
 
 function calculatePace(distance, time) {
     // Split the time string into hours, minutes, and seconds
@@ -36,7 +68,7 @@ function calculateTime(distance, speed) {
     return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     }
 
-    
+
 function calculateDistance(time, speed) {
     // Split the time string into hours, minutes, and seconds
     const [hours, minutes, seconds] = time.split(':');
