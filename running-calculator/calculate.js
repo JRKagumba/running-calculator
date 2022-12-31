65a8e86661266410672682bf11ff6310402d4852
JRKagumba <63820705+JRKagumba@users.noreply.github.com>
2022-12-31 07:44:33 -0500

    tried to access conversion functions

--- running-calculator/calculate.js
+++ running-calculator/calculate.js
@@ -1,4 +1,4 @@
-
+import * as convTools from 'convert.js';
 
 const dictionary = {
     "Marathon": 42200,
@@ -6,4 +6,18 @@ const dictionary = {
 }
 
 
-
+function calculateSpeed(distance, time) {
+    // Split the time string into minutes and seconds
+    const [minutes, seconds] = time.split(':');
+  
+    // Convert minutes and seconds to total seconds
+    const totalSeconds = (parseInt(minutes, 10) * 60) + parseInt(seconds, 10);
+  
+    // Calculate speed in meters per second
+    const speed = distance / totalSeconds;
+  
+    // Return the speed in kilometers per hour
+    return (speed * 3.6).toFixed(2);
+  }
+  
+  
