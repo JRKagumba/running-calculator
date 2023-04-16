b0faecef648e424404a2bcb66572bc80591909db
Joe Kagumba <63820705+JRKagumba@users.noreply.github.com>
2023-04-16 04:43:20 -0400

    Update dashboard.js
    Added a function to toggle the display of dashboard widgets.

--- running-calculator/js/dashboard.js
+++ running-calculator/js/dashboard.js
@@ -9,3 +9,20 @@ const loadDashboardWidgets = () => {
         dashboardWidgets.appendChild(widgetElement);
     });
 };
+
+
+const setupToggleWidgetsButton = () => {
+    const toggleWidgetsButton = document.getElementById("toggle-widgets");
+    toggleWidgetsButton.addEventListener("click", () => {
+        const dashboardWidgets = document.getElementById("dashboard-widgets");
+        const isHidden = dashboardWidgets.style.display === "none";
+        
+        if (isHidden) {
+            dashboardWidgets.style.display = "block";
+            toggleWidgetsButton.textContent = "Hide Widgets";
+        } else {
+            dashboardWidgets.style.display = "none";
+            toggleWidgetsButton.textContent = "Show Widgets";
+        }
+    });
+};
